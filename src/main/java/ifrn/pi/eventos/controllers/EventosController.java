2d16682108d63acc82e52f204dd0b53376782ef4
José Mércio <jose.mercio@escolar.ifrn.edu.br>
2026-09-15 19:18:45 -0300

    Apagando um evento da aplicação

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -85,4 +85,16 @@ public class EventosController {
 		return "redirect:/eventos/{idEvento}";
 
 	}
+
+	@GetMapping("/{id}/remover")
+	public String apagarEvento(@PathVariable Long id) {
+
+		Optional<Evento> opt = er.findById(id);
+		if (!opt.isEmpty()) {
+			Evento evento = opt.get();
+			er.delete(evento);
+		}
+		return "redirect:/eventos";
+
+	}
 }
